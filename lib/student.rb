193ff255921391436243a4f58259dfc1613bb311
nmhummel <natalie.hummel@gmail.com>
2020-11-20 17:16:05 -0600

    commit tree 1d12276b252da93b7d530615d1b93cc598a3bc48

--- lib/student.rb
+++ lib/student.rb
@@ -4,4 +4,12 @@ require 'interactive_record.rb'
 
 class Student < InteractiveRecord
 
+    self.column_names.each do |col_name|  # self.column_names method inherited from Superclass
+        attr_accessor col_name.to_sym
+    end
+
 end
+
+
+# Note: The only code the Student class needs to contain is the code to create the attr_accessors specific to itself. 
+# But even that code uses a method, #column_names, inherited from the super class.
